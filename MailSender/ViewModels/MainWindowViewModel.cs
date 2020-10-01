61ea8216067d60b21c2cb89a8cfbd317aaf699e7
Nika.P <pns9595@mail.ru>
2020-10-01 19:35:20 +0300

    Написана первая команда

--- MailSender/ViewModels/MainWindowViewModel.cs
+++ MailSender/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using WpfMailSender.ViewModels.Base;
+using WpfMailSender.Infrastructure.Commands;
+using System.Windows;
+using System.Windows.Input;
 
 namespace WpfMailSender.ViewModels
 {
@@ -15,5 +18,13 @@ namespace WpfMailSender.ViewModels
             set => Set(ref _Title, value);
         }
 
+        private ICommand _ShowDialogCommand;
+        public ICommand ShowDialogCommand => _ShowDialogCommand
+            ??= new LambdaCommand(OnShowDialogCommandExecuted);
+        private void OnShowDialogCommandExecuted(object p)
+        {
+            var message = p as string ?? "Hello World!";
+            MessageBox.Show(message, "Сообщение от первой команды");
+        }
     }
 }
