d84a2f57c9c22254a2d27aaa93d80009f49d5dcf
Nika.P <pns9595@mail.ru>
2020-10-08 20:46:38 +0300

    Реализация спам-рассылок

--- MailSender.lib/Services/SmtpMailService.cs
+++ MailSender.lib/Services/SmtpMailService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Net;
 using System.Net.Mail;
+using System.Threading;
 using MailSender.Interfaces;
 using WpfMailSender.Services;
 
@@ -18,8 +20,75 @@ namespace MailSender.Services
         {
             return new SmtpSender(Address, Port, UseSSL, Login, Password);
         }
+
         
     }
 
-    
+    internal class SmtpMailSender : IMailSender
+    {
+        private readonly string _Address;
+        private readonly int _Port;
+        private readonly bool _Ssl;
+        private readonly string _Login;
+        private readonly string _Password;
+
+        public SmtpMailSender(string Address, int Port, bool SSL, string Login, string Password)
+        {
+            _Address = Address;
+            _Port = Port;
+            _Ssl = SSL;
+            _Login = Login;
+            _Password = Password;
+        }
+
+        public void Send(string SenderAddress, string RecipientAddress, string Subject, string Body)
+        {
+            var from = new MailAddress(SenderAddress);
+            var to = new MailAddress(RecipientAddress);
+
+            using (var message = new MailMessage(from, to))
+            {
+                message.Subject = Subject;
+                message.Body = Body;
+
+                using (var client = new SmtpClient(_Address, _Port))
+                {
+                    client.EnableSsl = _Ssl;
+
+                    client.Credentials = new NetworkCredential
+                    {
+                        UserName = _Login,
+                        Password = _Password
+                    };
+
+                    try
+                    {
+                        client.Send(message);
+                    }
+                    catch (SmtpException e)
+                    {
+                        Trace.TraceError(e.ToString());
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public void Send(string SenderAddress, IEnumerable<string> RecipientsAddresses, string Subject, string Body)
+        {
+            foreach (var recipient_address in RecipientsAddresses)
+                Send(SenderAddress, recipient_address, Subject, Body);
+        }
+
+        public void SendParallel(string SenderAddress, IEnumerable<string> RecipientsAddresses, string Subject, string Body)
+        {
+            foreach (var recipient_address in RecipientsAddresses)
+                ThreadPool.QueueUserWorkItem(o => Send(SenderAddress, recipient_address, Subject, Body));
+        }
+
+        //[DllImport("file_name.dll")]
+        //private static extern void MethodName(string str);
+    }
+
+
 }
