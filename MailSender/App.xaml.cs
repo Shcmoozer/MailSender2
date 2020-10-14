fb008ec0624e7b62bda6d1ecdd28329820d94a50
Nika.P <pns9595@mail.ru>
2020-10-14 20:53:57 +0300

    Конфигурация приложения

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 using System.Windows;
 using MailSender.Interfaces;
 using MailSender.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using WpfMailSender.ViewModels;
 
 namespace MailSender
@@ -19,18 +21,24 @@ namespace MailSender
     public partial class App
     {
 
-        private static IHost __Hosting;
-        public static IHost Hosting
-        {
-            get
-            {
-                if (__Hosting != null) return __Hosting;
-                var host_builder = Host
-                    .CreateDefaultBuilder(Environment.GetCommandLineArgs());
-                host_builder.ConfigureServices(ConfigureServices);
-                return __Hosting = host_builder.Build();
-            }
-        }
+        private static IHost _Hosting;
+
+        public static IHost Hosting => _Hosting
+            ??= Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
+                .ConfigureHostConfiguration(cfg => cfg
+                    .AddJsonFile("appconfig.json", true, true)
+                    .AddXmlFile("appsettings.xml", true, true)
+                )
+                .ConfigureAppConfiguration(cfg => cfg
+                    .AddJsonFile("appconfig.json", true, true)
+                    .AddXmlFile("appsettings.xml", true, true)
+                )
+                .ConfigureLogging(log => log
+                    .AddConsole()
+                    .AddDebug()
+                )
+                .ConfigureServices(ConfigureServices)
+                .Build();
 
         public static IServiceProvider Services => Hosting.Services;
 
---
+++ MailSender/Data/MailSenderDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.Models;
+using Microsoft.EntityFrameworkCore;
+using WpfMailSender.Models;
+
+namespace WpfMailSender.Data
+{
+    class MailSenderDB : DbContext
+    {
+        public DbSet<Recipient> Recipients { get; set; }
+
+        public DbSet<Sender> Senders { get; set; }
+
+        public DbSet<Server> Servers { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+
+        //public DbSet<SchedulerTask> SchedulerTasks { get; set; }
+
+        public MailSenderDB(DbContextOptions<MailSenderDB> opt) : base(opt) { }
+
+        //protected override void OnModelCreating(ModelBuilder db)
+        //{
+        //    db.Entity<SchedulerTask>()
+        //       .HasMany(t => t.Recipients)
+        //       .WithOne("Task")
+        //       .OnDelete(DeleteBehavior.Cascade);
+        //}
+    }
+}
